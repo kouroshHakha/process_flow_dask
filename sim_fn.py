5afcbd0b5d6b1a1754ed8cd0e82c1d2bdb307f40
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2019-08-28 18:54:49 -0700

    implemented a preliminary sneak peak of how paralled task execution would work with dask

--- sim_fn.py
+++ sim_fn.py
@@ -1,41 +1,38 @@
 from typing import Dict, Any
 from time import sleep
+from dask.distributed import get_client
 
 def generate_layout(params: Dict[str, Any], lib_name: str, impl_name: str,
-                    ready=False):
-    print(f'generating layout {lib_name}__{impl_name} ...')
-    if not ready: return False
-    sleep(5)
-    print(f'layout {lib_name}__{impl_name} done.')
-    return True
+                    dep_list=None):
+    print(f'generating layout {lib_name}__{impl_name}_{params["nf"]} ...')
+    if params['nf'] == 3:
+        raise ValueError('cannot handle nf = 3')
+    sleep(1)
+    print(f'layout {lib_name}__{impl_name}_{params["nf"]} done.')
+    return params
 
 def generate_schematic(params: Dict[str, Any], lib_name: str, impl_name: str,
-                       ready=False):
-    print(f'generating schematic {lib_name}__{impl_name}, {ready}...')
-    if not ready: return False
-    sleep(2)
+                       dep_list=None):
+    print(f'generating schematic {lib_name}__{impl_name} ...')
+    sleep(1)
     print(f'schematic {lib_name}__{impl_name} done.')
     return True
 
-
-def run_lvs(lib_name: str, impl_name: str, ready=False):
-    if not ready: return False
+def run_lvs(lib_name: str, impl_name: str, dep_list=None):
     print(f'running lvs on {lib_name}__{impl_name}...')
-    sleep(3)
+    sleep(10)
     print(f'lvs on {lib_name}__{impl_name} done.')
     return True
 
-def run_drc(lib_name: str, impl_name: str, ready=False):
-    if not ready: return False
+def run_drc(lib_name: str, impl_name: str, dep_list=None):
     print(f'running drc on {lib_name}__{impl_name} ...')
-    sleep(5)
+    sleep(10)
     print(f'drc on {lib_name}__{impl_name} done.')
     return True
 
-def run_simulation(params: Dict[str, Any], lib_name: str, impl_name: str, ready=False):
+def run_simulation(params: Dict[str, Any], lib_name: str, impl_name: str, dep_list=None):
     sim_type = params['type']
     print(f'running simulation {sim_type} on {lib_name}__{impl_name}...')
-    if not ready: return False
 
     if sim_type == 'od':
         sleep(1)
@@ -49,31 +46,31 @@ def run_simulation(params: Dict[str, Any], lib_name: str, impl_name: str, ready=
     print(f'simulation {sim_type} on {lib_name}__{impl_name} done ...')
     return True
 
-def generate_cell(params: Dict[str, Any], lib_name: str, impl_name: str, client):
-    lay_done = client.submit(generate_layout, params, lib_name, impl_name, ready=True)
-    sch_done = client.submit(generate_schematic, params, lib_name, impl_name, ready=lay_done)
-    cell_done = lay_done and sch_done
-    return cell_done
+def generate_cell(params: Dict[str, Any], lib_name: str, impl_name: str, dep_list=None):
+    client = get_client()
+    sch_params = client.submit(generate_layout, params['layout_params'], lib_name, impl_name)
+    sch_done = client.submit(generate_schematic, sch_params, lib_name, impl_name)
+    return sch_done
 
-def create_designs(params, lib_name, impl_name, client):
-    lay_done = client.submit(generate_layout, params['layout_params'], lib_name, impl_name, True)
-    drc_done = client.submit(run_drc, lib_name, impl_name, ready=lay_done)
+def create_designs(params, lib_name, impl_name, dep_list=None):
+    client = get_client()
+    sch_params = client.submit(generate_layout, params['layout_params'], lib_name, impl_name)
+    drc_done = client.submit(run_drc, lib_name, impl_name, dep_list=sch_params)
 
     sch_done = {}
-    sch_done['main'] = client.submit(generate_schematic, params, lib_name, impl_name, ready=lay_done)
+    sch_done['main'] = client.submit(generate_schematic, sch_params, lib_name, impl_name,
+                                     dep_list=sch_params)
 
-    creation_done = client.submit(all, [sch_done['main'], lay_done])
-    lvs_done = client.submit(run_lvs, lib_name, impl_name, ready=creation_done)
+    lvs_done = client.submit(run_lvs, lib_name, impl_name, dep_list=sch_done['main'])
     for wrapper in params['wrapper_params']:
         sch_done[wrapper['name']] = client.submit(generate_schematic, wrapper, lib_name,
-                                                  wrapper['name'], ready=creation_done)
+                                                  wrapper['name'], dep_list=sch_done['main'])
 
     schs_done = client.submit(all, list(sch_done.values()))
     creation_done = client.submit(all, [schs_done, drc_done, lvs_done])
-
     return creation_done
 
-def verify_designs(params, lib_name, impl_name, client, ready=False):
+def verify_designs(params, lib_name, impl_name, ready=False):
     results = {}
     for tb_name, tb_params in params['sim_params'].items():
         tb_cell_name = f'tb_{tb_name}'
@@ -83,7 +80,9 @@ def verify_designs(params, lib_name, impl_name, client, ready=False):
     all_done = client.submit(all, [list(results.values())])
     return results, all_done
 
-def characterize(params: Dict[str, Any], lib_name: str, impl_name: str, client):
-    creation_done = create_designs(params, lib_name, impl_name, client)
-    results, all_done = verify_designs(params, lib_name, impl_name, client, ready=creation_done)
-    return results, all_done
+def characterize(params: Dict[str, Any], lib_name: str, impl_name: str):
+    client = get_client()
+    creation_done = create_designs(params, lib_name, impl_name)
+    # results, all_done = verify_designs(params, lib_name, impl_name, client, ready=creation_done)
+    # return results, all_done
+    return creation_done
--- test_dask.py
+++ test_dask.py
@@ -1,30 +1,43 @@
-from typing import Dict, Any
+from typing import Dict, Any, List
 from time import time
-from dask.distributed import Client
+from dask.distributed import Client, get_client, wait
+from copy import deepcopy
 
-from .sim_fn import characterize
+from sim_fn import characterize, generate_cell
 
 import pdb
 
-def emulate_flow(params: Dict[str, Any], client):
-
+def emulate_flow(params: Dict[str, Any]):
     dtsa_params = params['dtsa_params']
     top_params = params['top_params']
 
-    dtsa_res, dtsa_done = characterize(dtsa_params, 'TEST', 'dtsa', client)
-    top_res, top_done = characterize(top_params, 'TEST', 'top', client)
+    dtsa_done = characterize(dtsa_params, 'TEST', 'dtsa')
+    # top_done = characterize(top_params, 'TEST', 'top')
+    # done = client.submit(all, [dtsa_done, top_done])
+    return dtsa_done
+    # return dtsa_done
 
-    done = client.submit(all, [dtsa_done, top_done])
-    # all_res = list(dtsa_res.values()) + list(top_res.values())
-    # pdb.set_trace()
-    # tot_res = dict(dtsa=dtsa_res, top_res=top_res)
-    print(done.result())
+def run_sim(list_of_params: List[Dict[str, Any]]):
+    result_futures = []
+    for params in list_of_params:
+        result_futures.append(emulate_flow(params))
+
+    wait(result_futures)
+
+    results = []
+    for future in result_futures:
+        try:
+            res = future.result()
+        except:
+            res = future.exception()
+        results.append(res)
+
     pdb.set_trace()
-
 
 if __name__ == '__main__':
 
-    client = Client(processes=False)
+    # client = Client(processes=False)
+    client = Client(processes=True)
     params = dict(
         dtsa_params=dict(
             layout_params=dict(
@@ -49,5 +62,13 @@ if __name__ == '__main__':
     )
 
     s = time()
-    emulate_flow(params, client)
+
+    num_jobs = 5
+    list_of_params = []
+    for i in range(num_jobs):
+        new_params = deepcopy(params)
+        new_params['dtsa_params']['layout_params']['nf'] = i
+        new_params['top_params']['layout_params']['nf'] = i // 2
+        list_of_params.append(new_params)
+    run_sim(list_of_params)
     print(f'tasks finished in {time() - s:.4} seconds')
